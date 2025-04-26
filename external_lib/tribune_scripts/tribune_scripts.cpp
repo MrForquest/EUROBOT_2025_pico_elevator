eea1fd2ae9600fbd81b4cc94f0e22d0ac2bc7a51
MrForquest <folp228@gmail.com>
2025-04-26 16:27:12 +0300

    File pico_micro_ros_example.uf2 renamed to pico_elevator.uf2. Distance of approaching was raised till 22.5 cm

--- external_lib/tribune_scripts/tribune_scripts.cpp
+++ external_lib/tribune_scripts/tribune_scripts.cpp
@@ -82,7 +82,7 @@ void MoveController::collect()
     this->set_grippers(states0);
     sleep_ms(500);
     sms_sts_ptr->EnableTorque(1, 0);
-    this->go_dist(0.15, 0.075, 1);
+    this->go_dist(0.225, 0.075, 1);
     sleep_ms(500);
     this->set_visor(0, 100);
     sleep_ms(500);
@@ -91,7 +91,7 @@ void MoveController::collect()
     this->set_grippers(states);
     sleep_ms(500);
     this->lift(10 * STEPPER_6_MM);
-    this->go_dist(0.15, 0.1, -1);
+    this->go_dist(0.225, 0.1, -1);
 }
 
 void MoveController::set_waveshare_servo(u8 servo_id, s16 position_percents, u16 speed_percents, s16 start_v, s16 end_v, u8 acc = 50)
